301f3739744d95379d2a2bc2b6276e1902652f36
system8239999 <yh-yasuta@respect-pal.jp>
2019-04-08 12:44:27 +0900

    modified:   - index.php: $articlesが存在しない場合の処理を追加 deleted:   - edit/test.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require_once('./functions.php');
 
 // 記事一覧取得
 $res = callAPI('GET');
-$articles = $res['json']['articles'];
+$articles = isset($res['json']['articles'])? $res['json']['articles']: [];
 // テンプレートからView読み込み
 include('./template/index.php');
